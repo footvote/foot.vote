b3dfdd876247ef292099e9c6d2dde3be852eafc1
madmatvey <potehin@gmail.com>
2022-01-04 00:26:27 +0400

    docker backend+frontend

--- footvote-backend/config/initializers/cors.rb
+++ footvote-backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.site_url
+    origins Rails.application.credentials.site_url, 'footvote.xyz'
 
     resource '*',
       headers: :any,
