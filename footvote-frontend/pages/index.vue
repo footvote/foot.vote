a9cf774825e36a09e04770d952f5b28c6f2e40f0
madmatvey <potehin@gmail.com>
2022-01-04 21:17:25 +0400

    dev env ready + location request in index

--- footvote-frontend/pages/index.vue
+++ footvote-frontend/pages/index.vue
@@ -1,21 +1,52 @@
 <template>
   <div>
-    <div id="map-wrap" style="height: 100vh; width: 100%;">
-      <no-ssr>
-        <l-map :zoom="5" :center="[57, 75]">
+    <div id="map-wrap" style="height: 100vh">
+      <client-only>
+        <l-map ref="VoteMap" :zoom="this.zoom" :center="this.center">
           <l-tile-layer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
             attribution='&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'>
           </l-tile-layer>
         </l-map>
-      </no-ssr>
+      </client-only>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      zoom: 5,
+      center: [55.0229, 82.5604],
+      error: null,
+    };
+  },
   mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.showLocationOnTheMap(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+        },
+        (error) => {
+          this.error =
+            "Locator is unable to find your address. Please type your address manually.";
+          // console.log(error.message);
+        }
+      );
+    } else {
+      this.error = error.message;
+      console.log("Your browser does not support geolocation API ");
+    }
+  },
+  methods: {
+    showLocationOnTheMap(latitude, longitude) {
+        this.zoom = 8
+        this.center = [latitude, longitude]
+    },
   }
 }
 </script>
