33eec6fd73e701d08ea2667e226b922edb8dad89
madmatvey <potehin@gmail.com>
2022-01-08 00:05:06 +0400

    switch to sql schema

--- footvote-backend/config/application.rb
+++ footvote-backend/config/application.rb
@@ -5,11 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
@@ -24,12 +20,10 @@ module FootvoteBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Moscow"
+
+    config.active_record.schema_format = :sql
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
